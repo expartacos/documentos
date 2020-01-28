da8c50fc3a80fdfc25dd9235303bbc1cb83d18dd
Rafael Pereira <rafaelpersan@gmail.com>
2020-01-27 21:42:02 -0300

    Adicionando Firebase

--- js/documentos.js
+++ js/documentos.js
@@ -4,5 +4,14 @@ function search(){
     validation(cpf);
 }
 function validation(cpfValue){
+    
+    document.getElementById('busca').setAttribute("class" , "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML= 'Certificados de: ' +cpfValue;
+}
+
+function back(){
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class" , "ocultar");
+    document.getElementById('inputCPF').value = '';
 }
---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDitSCFfHTfY616dfnEcXl9G_kp90ajghM",
+    authDomain: "documentos-5365a.firebaseapp.com",
+    databaseURL: "https://documentos-5365a.firebaseio.com",
+    projectId: "documentos-5365a",
+    storageBucket: "documentos-5365a.appspot.com",
+    messagingSenderId: "829937002203",
+    appId: "1:829937002203:web:21f7a62037d24802e4cefe"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
