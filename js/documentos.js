947d52d041cba8c0dea7a2a602b5f062e79cdc9c
Rafael Pereira <rafaelpersan@gmail.com>
2020-01-28 20:48:31 -0300

    Busca da Pasta por cpf

--- js/documentos.js
+++ js/documentos.js
@@ -4,12 +4,30 @@ function search(){
     validation(cpf);
 }
 function validation(cpfValue){
-    
+ 
     document.getElementById('busca').setAttribute("class" , "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML= 'Certificados de: ' +cpfValue;
+
+    var storage = firebase.storage();
+    //Retorna uma promisses que será processada
+    var resultado = storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        if(todosArquivos.items.length >= 1){
+            alert('CPF Encontrado');
+        } else{
+            alert('CPF não encontrado');
+        }
+    }).catch(function(error){
+        console.log('ERRO', error);
+    });
 }
 
+function next(cpfValue){
+    document.getElementById('busca').setAttribute("class" , "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
+    document.getElementById('tituloDocumentos').innerHTML= 'Certificados de: ' +cpfValue;
+
+}
 function back(){
     document.getElementById('busca').removeAttribute("class", "ocultar");
     document.getElementById('resultado').setAttribute("class" , "ocultar");
