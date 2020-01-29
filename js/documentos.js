832ee49d222beb354567687919d185254ecd58ee
Rafael Pereira <rafaelpersan@gmail.com>
2020-01-28 21:04:52 -0300

    upando

--- js/documentos.js
+++ js/documentos.js
@@ -13,7 +13,8 @@ function validation(cpfValue){
     //Retorna uma promisses que será processada
     var resultado = storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
         if(todosArquivos.items.length >= 1){
-            alert('CPF Encontrado');
+            listFiles(cpfValue);
+            next(cpfValue);
         } else{
             alert('CPF não encontrado');
         }
@@ -22,10 +23,19 @@ function validation(cpfValue){
     });
 }
 
+function listFiles(cpfValue){
+    document.getElementById('tituloDocumentos').innerHTML= 'Certificados de: ' +cpfValue;
+    var storage = firebase.storage();
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        
+    });
+
+}
+
 function next(cpfValue){
     document.getElementById('busca').setAttribute("class" , "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
-    document.getElementById('tituloDocumentos').innerHTML= 'Certificados de: ' +cpfValue;
+    
 
 }
 function back(){
